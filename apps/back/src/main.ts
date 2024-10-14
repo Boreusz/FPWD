a9ba87c42a98ccb7d24dc1994a502034c79adb38
Mateusz <matt.borowczyk@gmail.com>
2024-10-14 19:23:42 +0200

    delete back

--- apps/back/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { BackModule } from './back.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(BackModule);
-  await app.listen(3000);
-}
-bootstrap();
